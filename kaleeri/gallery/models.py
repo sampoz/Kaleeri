9b4268aecd841f378280556ac6f6be8659f078cb
AM Helin <am@myfrank.fi>
2014-01-25 13:41:08 +0200

    More project skeleton

---
+++ kaleeri/gallery/models.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from django.db import models
+
+
+class Gallery(models.Model):
+    pass
+
+
+class Photo(models.Model):
+    pass
---
+++ kaleeri/gallery/views.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+from django.db import views
