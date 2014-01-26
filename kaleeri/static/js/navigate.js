d0fc309ed05dac9f04b1cdf703ba87ac70f19702
sampoz <sampsa.laapotti@iki.fi>
2014-01-26 17:33:24 +0200

    Added Jquery fades, Came in my pants a bit

--- kaleeri/static/js/navigate.js
+++ kaleeri/static/js/navigate.js
@@ -6,35 +6,36 @@ window.Kaleeri = {
     }
 };
 var state = {
-    "view" : "main",
-    "parameter" : ""
-}
+    "view": "main",
+    "parameter": ""
+};
 
 window.addEventListener("popstate", function(e) {
-    console.log(event.state);
+    console.log(location.hash);
     loadState(event.state);
 });
 
 function loadState(stateToLoad){
     console.log("loading"+ history.state.view +" and next state is"+ stateToLoad.view);
+    console.log(stateToLoad);
+
     switch (stateToLoad.view){
         case "main" : {
             console.log("loading main");
             return;
         }
         case "album" :{
-            console.log("loading album")
+            console.log("loading album");
             Kaleeri.loadAlbums();
             return;
         }
         case "albumPhotos":{
             console.log("loading album photos");
-            Kaleeri.loadAlbumPhotos(1,1);
+            Kaleeri.loadAlbumPhotos(stateToLoad.parameter.albumId,stateToLoad.parameter.pageNumber);
             return;
         }
     }
 }
-
 $(function() {
     $(document.createElement("div")).load(
         "/static/handlebar-templates.html",
@@ -46,28 +47,39 @@ $(function() {
     );
 });
 
-Kaleeri.loadAlbums= function() {
-    $(document).ready(function(){
+Kaleeri.loadAlbums = function () {
+    $(document).ready(function () {
         $.getJSON("album/list", function (data) {
             var source = Kaleeri.templates.album_front;
             var template = Handlebars.compile(source);
             var html = template(data);
             $("#content-placeholder").html(html);
-            state.view="album";
-            history.pushState(state,"Albums bitches","");
+            state.view = "album";
+            history.pushState(state, " ", "#album/");
         })
     });
-}
-Kaleeri.loadAlbumPhotos= function(albumId,pageNumber) {
-    $(document).ready(function(){
-        $.getJSON("album/"+albumId+"/page/"+pageNumber, function (data) {
+};
+
+Kaleeri.fadeOutAlbums = function (albumId,pageNumber) {
+    console.log("lol");
+    $("#content-placeholder").fadeOut(300, function(){
+        Kaleeri.loadAlbumPhotos(albumId,pageNumber);
+    });
+};
+
+Kaleeri.loadAlbumPhotos = function (albumId, pageNumber) {
+    $(document).ready(function () {
+        $.getJSON("album/" + albumId + "/page/" + pageNumber, function (data) {
             var source = Kaleeri.templates.album_view;
             var template = Handlebars.compile(source);
             var html = template(data);
-            $("#content-placeholder").html(html);
+            $("#content-placeholder").html(html).fadeIn();
+            state.view = "albumPhotos";
+            console.log("Setting parameters "+albumId +" and "+pageNumber)
+            state.parameter.albumId = albumId;
+            state.parameter.pageNumber = pageNumber;
+            history.pushState(state, " ", "#album/" + albumId + "/page/" + pageNumber);
 
-            state.view="albumPhotos";
-            history.pushState(state,"Albums bitches","");
         })
-    });
-}
+    })
+};
