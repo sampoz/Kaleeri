f91286cd8332ed7abb9155e697e0603636170a8e
sampoz <sampsa.laapotti@iki.fi>
2014-02-09 23:37:32 +0200

    preliminary flicker integration works almost :D

--- kaleeri/static/js/navigate.js
+++ kaleeri/static/js/navigate.js
@@ -1,8 +1,8 @@
 window.Kaleeri = {
     "templates": {},
     "urls": {
-        create_album:"/",
-        add_picture:""
+        create_album: "/",
+        add_picture: ""
     },
     "state": "main"
 };
@@ -11,62 +11,67 @@ var state = {
     "parameter": ""
 };
 
-window.addEventListener("popstate", function(e) {
+window.addEventListener("popstate", function (e) {
     console.log(location.hash);
     loadState(event.state);
 });
 
-function loadState(stateToLoad){
-    console.log("loading"+ history.state.view +" and next state is"+ stateToLoad.view);
+
+
+function loadState(stateToLoad) {
+    console.log("loading" + history.state.view + " and next state is" + stateToLoad.view);
     console.log(stateToLoad);
 
-    switch (stateToLoad.view){
-        case "main" : {
+    switch (stateToLoad.view) {
+        case "main" :
+        {
             console.log("loading main");
             return;
         }
-        case "album" :{
+        case "album" :
+        {
             console.log("loading album");
             Kaleeri.loadAlbums();
             return;
         }
-        case "albumPhotos":{
+        case "albumPhotos":
+        {
             console.log("loading album photos");
-            Kaleeri.loadAlbumPhotos(stateToLoad.parameter.albumId,stateToLoad.parameter.pageNumber);
+            Kaleeri.loadAlbumPhotos(stateToLoad.parameter.albumId, stateToLoad.parameter.pageNumber);
             return;
         }
     }
 }
-$(function() {
+$(function () {
     $(document.createElement("div")).load(
         "/static/handlebar-templates.html",
         function () {
-            $(this).find("script").each(function(i, e) {
-                Kaleeri.templates[e.id]= e.innerHTML;
+            $(this).find("script").each(function (i, e) {
+                Kaleeri.templates[e.id] = e.innerHTML;
             });
         }
     );
 });
 
-Kaleeri.nextPage = function(){
+Kaleeri.nextPage = function () {
     console.log(Kaleeri.state);
-    Kaleeri.fadeOutAlbums(Kaleeri.state.parameter.albumId, parseInt(Kaleeri.state.parameter.pageNumber)+1);
+    Kaleeri.fadeOutAlbums(Kaleeri.state.parameter.albumId, parseInt(Kaleeri.state.parameter.pageNumber) + 1);
     Kaleeri.fadeInAlbums();
 };
 
-Kaleeri.previousPage = function(){
+Kaleeri.previousPage = function () {
     console.log(Kaleeri.state);
-    Kaleeri.fadeOutAlbums(Kaleeri.state.parameter.albumId, parseInt(Kaleeri.state.parameter.pageNumber)-1);
+    Kaleeri.fadeOutAlbums(Kaleeri.state.parameter.albumId, parseInt(Kaleeri.state.parameter.pageNumber) - 1);
     Kaleeri.fadeInAlbums();
 };
 
-Kaleeri.fadeInAlbums= function(){
+Kaleeri.fadeInAlbums = function () {
     $("#content-placeholder").fadeIn();
 };
 
 Kaleeri.loadAlbums = function () {
     $(document).ready(function () {
-        $.getJSON(window.location.origin+"/album/list", function (data) {
+        $.getJSON(window.location.origin + "/album/list", function (data) {
             var source = Kaleeri.templates.album_front;
             var template = Handlebars.compile(source);
             var html = template(data);
@@ -77,24 +82,24 @@ Kaleeri.loadAlbums = function () {
     });
 };
 
-Kaleeri.fadeOutAlbums = function (albumId,pageNumber) {
-    $("#content-placeholder").fadeOut(300, function(){
-        Kaleeri.loadAlbumPhotos(albumId,pageNumber);
+Kaleeri.fadeOutAlbums = function (albumId, pageNumber) {
+    $("#content-placeholder").fadeOut(300, function () {
+        Kaleeri.loadAlbumPhotos(albumId, pageNumber);
     });
 };
 
-Kaleeri.addPhoto = function() {
+Kaleeri.addPhoto = function () {
     $(document).ready(function () {
 
-            var source = Kaleeri.templates.add_photo;
-            var template = Handlebars.compile(source);
-            var html = template();
-            $("#content-placeholder").html(html);
+        var source = Kaleeri.templates.add_photo;
+        var template = Handlebars.compile(source);
+        var html = template();
+        $("#content-placeholder").html(html);
     })
 };
 
-Kaleeri.modifyPhoto = function() {
-    $(document).ready(function() {
+Kaleeri.modifyPhoto = function () {
+    $(document).ready(function () {
         $.post($("add.html", "#url").serialize());
         var source = Kaleeri.templates.modify_photo;
         var template = Handlebars.compile(source);
@@ -103,13 +108,13 @@ Kaleeri.modifyPhoto = function() {
     })
 }
 
-Kaleeri.photoToAlbum = function() {
+Kaleeri.photoToAlbum = function () {
 
 }
 
 Kaleeri.loadAlbumPhotos = function (albumId, pageNumber) {
     $(document).ready(function () {
-        $.getJSON("album/"+albumId +"/", function(data2) {
+        $.getJSON("album/" + albumId + "/", function (data2) {
             var source2 = Kaleeri.templates.album_details;
             var template2 = Handlebars.compile(source2);
             var html2 = template2(data2);
@@ -121,14 +126,14 @@ Kaleeri.loadAlbumPhotos = function (albumId, pageNumber) {
             var template = Handlebars.compile(source);
             var html = template(data);
             $("#content-placeholder").append(html);
-            
+
             Kaleeri.fadeInAlbums();
             state.view = "albumPhotos";
-            console.log("Setting parameters "+albumId +" and "+pageNumber)
-            state.parameter={};
+            console.log("Setting parameters " + albumId + " and " + pageNumber)
+            state.parameter = {};
             state.parameter.albumId = String(albumId);
             state.parameter.pageNumber = String(pageNumber);
-            Kaleeri.state=state;
+            Kaleeri.state = state;
             history.pushState(state, " ", "#album/" + albumId + "/page/" + pageNumber);
 
         })
